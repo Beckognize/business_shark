33031b291c4743ad94c932e4095ed5ce0f5a2822
Rebecca Lessem <beckognize@gmail.com>
2021-02-23 15:21:52 +0000

    added content to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-<h1>Hello Did this work?</h1>
+import Header from '../components/header';
+import Link from 'next/link'
+export default () => {
+    return (
+      
+      <div className="container-fluid text-center">
+        <Header></Header>
+        <h1>did this work?</h1>
+       
+      </div>
+    );
+  }
